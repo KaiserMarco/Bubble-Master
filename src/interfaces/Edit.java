1ba709277acffa27c45c13cabce5acfcea81e514
cecco <cecco@192.168.1.106>
2016-12-21 17:44:36 +0100

    Better.

--- src/interfaces/Edit.java
+++ src/interfaces/Edit.java
@@ -797,7 +797,8 @@ public class Edit
 							if(temp.getID().equals( Global.PLAYER ))
 								updateItemPlayer( (Player) temp, false );
 
-							if(temp.getID().equals( Global.TUBO ))
+							temp = null;
+							if(ostacoli.get( ostacoli.size() - 1 ).getID().equals( Global.TUBO ))
 							    {
 									// inserisce una coppia nuova di tubi
 									if(nuovoTubo)
@@ -812,12 +813,8 @@ public class Edit
 										{
 											ostacoli.get( ostacoli.size() - 1 ).setUnion( indexFirstTube );
 											ostacoli.get( indexFirstTube ).setUnion( ostacoli.size() - 1 );
-											
-											temp = null;
 										}
 							    }
-							else
-								temp = null;
 						}
 				}
 			//se NON ho un elemento da inserire
